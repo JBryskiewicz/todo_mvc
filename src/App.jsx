3094bdf361cbc319b4a645dcfcd65ba6fc8d53df
JBryskiewicz <kuba@fiku.eu>
2023-05-29 13:00:14 +0200

    Todo cread from CRUD for our list maker

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
-  return <></>
+    const [value, setValue] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const getId = () => {
+        if (!tasks.length) return 1;
+
+        return Math.max(...tasks.map((task) => task.id)) + 1;
+    }
+
+    const handleAddTask = (event) => {
+        if (event.key === "Enter") {
+            setTasks(prevState => [...prevState, {
+            id: getId(),
+            name: value,
+            status: 'active'
+            }]);
+            setValue('');
+        }
+    }
+
+    // const handleDelete = (event) => {
+    //
+    // }
+
+    const taskList = tasks.map((element) => (
+        <li key={ element.id }>
+            { element.name }
+            <button>X</button>
+        </li>
+    ));
+
+    return (
+        <>
+            <h1>Todos</h1>
+            <input type="text"
+                   value={value}
+                   onChange={event => setValue(event.target.value)}
+                   onKeyUp={handleAddTask}
+            />
+            <ul>
+                { taskList }
+            </ul>
+        </>
+    )
 }
 
 export default App
