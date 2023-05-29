2e1c524ef6e0fc0070e2f5dbe9e8e9fbee129a77
JBryskiewicz <kuba@fiku.eu>
2023-05-29 15:38:30 +0200

    delete done button functionality implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,80 @@
-import './App.css'
 import {useState} from "react";
+import "./App.css";
 
 function App() {
     const [value, setValue] = useState('');
     const [tasks, setTasks] = useState([]);
 
-    const getId = () => {
-        if (!tasks.length) return 1;
 
-        return Math.max(...tasks.map((task) => task.id)) + 1;
+    function getId() {
+        if (!tasks.length) return 1;
+        return Math.max(...tasks.map((task) => task.id)) + 1
     }
 
-    const handleAddTask = (event) => {
-        if (event.key === "Enter") {
-            setTasks(prevState => [...prevState, {
+    function handleInput(event) {
+        setValue(event.target.value);
+    }
+
+    function handleAddTask(event) {
+        if (event.key === 'Enter') {
+            setTasks([...tasks, {
                 id: getId(),
                 name: value,
                 status: 'active'
             }]);
             setValue('');
         }
+
     }
 
-    const handleDelete = (toDelete) => {
+    function handleDelete(taskToDelete) {
         return () => {
-            const newTasks = tasks.filter((task) => task !== toDelete);
+            const newTasks = tasks.filter((task) => task !== taskToDelete);
             setTasks(newTasks);
         }
     }
 
-    const handleChangeStatus = (task) => {
-        return () => {
+    function handleChangeStatus(task) {
+        return function () {
             task.status = task.status === 'active' ? 'done' : 'active';
-            setTasks([...tasks]);
+            setTasks([...tasks])
         }
     }
 
-    const taskList = tasks.map((element) => (
-        <li key={element.id}>
-            <button
-                onClick={handleChangeStatus(element)}
-                className={element.status === 'active' ? 'active' : 'done'}>
-                { element.status }
-            </button>
-            <span>{element.name}</span>
-            <button
-                onClick={handleDelete(element)}>
-                X
-            </button>
-        </li>
-    ));
+    function handleDeleteDoneTasks(){
+        setTasks(tasks.filter((task) => task.status !== 'done'));
+    }
 
     return (
         <>
-            <h1>Todos</h1>
-            <input type="text"
-                   value={value}
-                   onChange={event => setValue(event.target.value)}
-                   onKeyUp={handleAddTask}
+            <h1>todos</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={handleInput}
+                onKeyUp={handleAddTask}
             />
             <ul>
-                {taskList}
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <button
+                            onClick={handleChangeStatus(task)}
+                            className={task.status === 'active' ? 'active' : 'done'}
+                        >{task.status}</button>
+                        <span>{task.name}</span>
+                        <button
+                            onClick={handleDelete(task)}
+                        >delete
+                        </button>
+                    </li>
+                ))}
             </ul>
+
+            {tasks.some((task) => task.status === 'done') && (
+                <button
+                    onClick={handleDeleteDoneTasks}
+                >Clear completed</button>
+            )}
         </>
     )
 }
